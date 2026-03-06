6ef354f64f3aca4f1d6f561bcd26d73effb265e1
JoaoPresley <jvalencarmg@gmail.com>
2026-03-05 22:27:41 -0400

    adicionado controllers no HelloController

--- src/main/java/com/example/cursobasicojavafx/HelloController.java
+++ src/main/java/com/example/cursobasicojavafx/HelloController.java
@@ -1,14 +1,48 @@
 package com.example.cursobasicojavafx;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button bt_deletar;
+
+    @FXML
+    private Button bt_editar;
+
+    @FXML
+    private Button bt_salvar;
+
+    @FXML
+    private RadioButton rb_F;
+
+    @FXML
+    private RadioButton rb_M;
+
+    @FXML
+    private TableColumn<?, ?> tc_id;
+
+    @FXML
+    private TableColumn<?, ?> tc_idade;
+
+    @FXML
+    private TableColumn<?, ?> tc_nome;
+
+    @FXML
+    private TableColumn<?, ?> tc_sexo;
+
+    @FXML
+    private TextField tf_idade;
+
+    @FXML
+    private TextField tf_nome;
+
+    @FXML
+    private TableView<?> tv_estudante;
+
 }
