45f9a5f5d828fbe3f2ee958702e047da79ef319a
JoaoPresley <jvalencarmg@gmail.com>
2026-03-06 19:07:39 -0400

    Inserindo driver para o PostgreSQL

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.example.cursobasicojavafx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires org.postgresql.jdbc;
 
     opens com.example.cursobasicojavafx to javafx.fxml;
     exports com.example.cursobasicojavafx;
